73ec1bf89950cd330c96480667d616780376f51b
Assas <Assas@DESKTOP-5URUDDL>
2025-12-04 23:01:57 -0600

    Avance PC Dic 4 2025

--- src/main/java/com/mycompany/proyectopoo/GestionNegocios.java
+++ src/main/java/com/mycompany/proyectopoo/GestionNegocios.java
@@ -21,7 +21,6 @@ public class GestionNegocios {
 //        negocios[5] = new Negocio();
 //
 //    }
-
     // Submenú de Gestión de Negocios y bucle del menu
     public void menuGestionNegocios() {
 
@@ -60,7 +59,7 @@ public class GestionNegocios {
                     break;
 
                 case "3": // Editar
-                    JOptionPane.showMessageDialog(null, "Opción no disponible aún");
+                    editarNegocio(negocios);
                     break;
                 case "4": // Eliminar
                     JOptionPane.showMessageDialog(null, "Opción no disponible aún");
@@ -400,12 +399,27 @@ public class GestionNegocios {
             return;
         }
 
+        // === Variables de la clase ===
+        String nuevoNombre = null;
+        TipoNegocio tipoNegocio = null;
+        String nuevoContacto = null;
+        String telefonoNegocio = null;
+        String correoNegocio = null;
+        String direccionNegocio = null;
+        double limiteCredito = 0.0;
+
+        // === Variables de validación del formato ===
+        boolean error_correoNegocio = false;
+        boolean error_telefonoNegocio = false;
+        boolean error_credito = false;
+
         String codigoBuscar = JOptionPane.showInputDialog("""
                                                           Ingrese el código del repuesto a editar
                                                           Formato: R###
                                                           """);
 
         for (int i = 0; i < Negocio.getCantidad(); i++) {
+
             if (negocios[i].getCodigoNegocio().equalsIgnoreCase(codigoBuscar)) {
 
                 int opt = Integer.parseInt(JOptionPane.showInputDialog(
@@ -420,73 +434,201 @@ public class GestionNegocios {
                         + "\n8. Regresar"
                 ));
 
-                switch (opt) {
-                    case 1: // Nuevo nombre
-                        String nuevoNombre = JOptionPane.showInputDialog("Ingrese el nombre del producto");
-                        negocios[i].setNombreNegocio(nuevoNombre);
-                        JOptionPane.showMessageDialog(null, "Nombre actualizado con éxito a " + negocios[i].getNombreNegocio());
-                        opt = 8;
-                        break;
-                    case 2: // Nuevo tipo
+                do {
+                    switch (opt) {
 
-                        TipoNegocio tipoNegocio = null;
-                        String optBotones[] = {"Gasolinera", "Taller", "Venta de repuestos", "Alquiler de vehículos"};
-                        int optCatFiltrada = JOptionPane.showOptionDialog(
-                                null,
-                                "Elija el tipo de negocio",
-                                "FILTRO DE NEGOCIO",
-                                JOptionPane.DEFAULT_OPTION,
-                                JOptionPane.QUESTION_MESSAGE,
-                                null,
-                                optBotones,
-                                "Gasolinera");
+                        case 1: // Nuevo nombre
+                            nuevoNombre = JOptionPane.showInputDialog("Ingrese el nombre del producto");
+                            negocios[i].setNombreNegocio(nuevoNombre);
+                            JOptionPane.showMessageDialog(null, "Nombre actualizado con éxito a " + negocios[i].getNombreNegocio());
+                            break;
 
-                        switch (optCatFiltrada) {
-                            case 0:
-                                tipoNegocio = TipoNegocio.Gasolinera;
-                                break;
-                            case 1:
-                                tipoNegocio = TipoNegocio.Taller;
-                                break;
-                            case 2:
-                                tipoNegocio = TipoNegocio.Venta_Repuestos;
-                                break;
-                            case 3:
-                                tipoNegocio = TipoNegocio.Alquiler_Vehiculos;
-                                break;
-                            // OptGestion invalida
-                            default:
-                                JOptionPane.showMessageDialog(null, "Opción no valida\nElegido por defecto (Gasolinera)");
-                        }
+                        case 2: // Nuevo tipo
 
-                        break;
+                            tipoNegocio = null;
+                            String optBotones[] = {"Gasolinera", "Taller", "Venta de repuestos", "Alquiler de vehículos"};
+                            int optCatFiltrada = JOptionPane.showOptionDialog(
+                                    null,
+                                    "Elija el tipo de negocio",
+                                    "FILTRO DE NEGOCIO",
+                                    JOptionPane.DEFAULT_OPTION,
+                                    JOptionPane.QUESTION_MESSAGE,
+                                    null,
+                                    optBotones,
+                                    "Gasolinera");
 
-                    case 3: // Contacto
-                        String nuevoContacto = JOptionPane.showInputDialog("Ingrese el nombre del producto");
-                        negocios[i].setContactoNegocio(nuevoContacto);
-                        JOptionPane.showMessageDialog(null, "Nombre del contacto ha sido actualizado con éxito a " + negocios[i].getContactoNegocio());
-                        opt = 8;
-                        break;
-                    case 8:
-                        JOptionPane.showMessageDialog(null, "Volviendo al menú de Gestión de Repuestos");
-                        break;
+                            switch (optCatFiltrada) {
+                                case 0:
+                                    tipoNegocio = TipoNegocio.Gasolinera;
+                                    break;
+                                case 1:
+                                    tipoNegocio = TipoNegocio.Taller;
+                                    break;
+                                case 2:
+                                    tipoNegocio = TipoNegocio.Venta_Repuestos;
+                                    break;
+                                case 3:
+                                    tipoNegocio = TipoNegocio.Alquiler_Vehiculos;
+                                    break;
+                                // OptGestion invalida
+                                default:
+                                    JOptionPane.showMessageDialog(null, "Opción no valida\nElegido por defecto (Gasolinera)");
+                            }
 
-                    default:
-                        JOptionPane.showMessageDialog(null, "Opción no valida, intente de nuevo");
-                        opt = Integer.parseInt(JOptionPane.showInputDialog(
-                                "=== Menu de actualización ==="
-                                + "\n1. Nombre (Actual: " + negocios[i].getNombreNegocio() + ")"
-                                + "\n2. Tipo de negocio (Actual: " + negocios[i].getTipoNegocio() + ")"
-                                + "\n3. Contacto (Actual: " + negocios[i].getContactoNegocio() + ")"
-                                + "\n4. Teléfono (Actual: " + negocios[i].getTelefonoNegocio() + ")"
-                                + "\n5. Correo (Actual: " + negocios[i].getCorreoNegocio() + ")"
-                                + "\n6. Dirección (Actual: " + negocios[i].getDireccionNegocio() + ")"
-                                + "\n7. Límite de crédito ($) (Actual: " + negocios[i].getLimiteCreditoNegocio() + ")"
-                                + "\n8. Regresar"
-                        ));
+                            negocios[i].setTipoNegocio(tipoNegocio);
+                            JOptionPane.showMessageDialog(null, "Tipo de negocio de " + negocios[i].getNombreNegocio() + "actualizado con éxito a " + negocios[i].getTipoNegocio());
+                            break;
 
-                }
+                        case 3: // Contacto
+                            nuevoContacto = JOptionPane.showInputDialog("Ingrese el nombre del producto");
+                            negocios[i].setContactoNegocio(nuevoContacto);
+                            JOptionPane.showMessageDialog(null, "Nombre del contacto ha sido actualizado con éxito a " + negocios[i].getContactoNegocio());
+                            break;
+
+                        case 4: // Telefono
+
+                            do {
+                                telefonoNegocio = JOptionPane.showInputDialog("""
+                                                    "Ingrese el número de teléfono del negocio 
+                                                    Siga el siguiente formato '####-####'
+                                                     """);
+                                if (telefonoNegocio.length() != 9) {
+                                    error_telefonoNegocio = true;
+                                } else {
+
+                                    if (telefonoNegocio.charAt(4) == '-') {
+
+                                        for (int j = 0; j < telefonoNegocio.length(); j++) {
+
+                                            if (j != 4) {
+                                                if (!(telefonoNegocio.charAt(j) >= '0' && telefonoNegocio.charAt(j) <= '9')) {
+                                                    error_telefonoNegocio = true;
+                                                }
+                                            }
+                                        }
+                                    } else {
+                                        error_telefonoNegocio = true;
+                                    }
+                                }
+
+                                if (error_telefonoNegocio) {
+                                    error_telefonoNegocio = false;
+                                    telefonoNegocio = JOptionPane.showInputDialog("""
+                                                    Formato incorrecto
+                                                    Ingrese de nuevo el número de teléfono del negocio 
+                                                    Siga el siguiente formato '####-####'
+                                                     """);
+
+                                    if (telefonoNegocio.length() != 9) {
+                                        error_telefonoNegocio = true;
+                                    } else {
+
+                                        if (telefonoNegocio.charAt(4) == '-') {
+
+                                            for (int j = 0; j < telefonoNegocio.length(); j++) {
+
+                                                if (j != 4) {
+                                                    if (!(telefonoNegocio.charAt(j) >= '0' && telefonoNegocio.charAt(j) <= '9')) {
+                                                        error_telefonoNegocio = true;
+                                                    }
+                                                }
+                                            }
+                                        } else {
+                                            error_telefonoNegocio = true;
+                                        }
+                                    }
+                                } else {
+                                    negocios[i].setTelefonoNegocio(telefonoNegocio);
+                                    JOptionPane.showMessageDialog(null, "Nombre número del negocio ha sido actualizado con éxito a " + negocios[i].getTelefonoNegocio());
+                                    break;
+                                }
+                            } while (error_telefonoNegocio == true);
+
+                        case 5: // Correo
+
+                            do {
+                                correoNegocio = JOptionPane.showInputDialog("Ingrese el correo del negocio");
+                                if (error_correoNegocio) {
+                                    error_correoNegocio = false;
+                                    correoNegocio = JOptionPane.showInputDialog("Ingrese el correo del negocio");
+
+                                    int ubicacionArroba = -1;
+                                    int ubicacionPunto = -1;
+
+                                    for (int j = 0; j < correoNegocio.length(); j++) {
+                                        if (correoNegocio.charAt(j) == '@') {
+                                            ubicacionArroba = j;
+
+                                            for (int k = 0; k < correoNegocio.length(); k++) {
+                                                if (correoNegocio.charAt(k) == '.') {
+                                                    ubicacionPunto = k;
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    if (ubicacionArroba <= 0 || ubicacionPunto < ubicacionArroba
+                                            || ubicacionPunto == correoNegocio.length() - 1
+                                            || ubicacionPunto <= ubicacionArroba + 1) {
+                                        error_correoNegocio = true;
+                                    }
+
+                                    if (ubicacionArroba == -1 || ubicacionPunto == -1) {
+                                        error_correoNegocio = true;
+                                    }
+                                } else {
+                                    negocios[i].setCorreoNegocio(correoNegocio);
+                                    JOptionPane.showMessageDialog(null, "Nombre correo del negocio ha sido actualizado con éxito a " + negocios[i].getCorreoNegocio());
+                                    break;
+                                }
+                            } while (error_correoNegocio == true);
+
+                        case 6: // Dirección
+                            direccionNegocio = JOptionPane.showInputDialog("Ingrese la nueva dirección de " + negocios[i].getNombreNegocio());
+                            negocios[i].setDireccionNegocio(direccionNegocio);
+                            JOptionPane.showMessageDialog(null, "La direción del negocio ha sido actualizada con éxito a " + negocios[i].getDireccionNegocio());
+                            break;
+
+                        case 7:
+                            do {
+                                limiteCredito = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el nuevo límite de crédito para " + negocios[i].getNombreNegocio()));
+
+                                if (limiteCredito < 1000 || limiteCredito > 8000) {
+                                    error_credito = true;
+                                } else {
+                                    negocios[i].setLimiteCreditoNegocio(limiteCredito);
+                                    JOptionPane.showMessageDialog(null, "El límite del crédito del negocio ha sido actualizado con éxito a " + negocios[i].getLimiteCreditoNegocio());
+                                    break;
+                                }
+
+                            } while (error_credito == true);
+
+                        case 8:
+                            JOptionPane.showMessageDialog(null, "Volviendo al menú de Gestión de Repuestos");
+                            break;
+
+                        default:
+                            JOptionPane.showMessageDialog(null, "Opción no valida, intente de nuevo");
+                            opt = Integer.parseInt(JOptionPane.showInputDialog(
+                                    "=== Menu de actualización ==="
+                                    + "\n1. Nombre (Actual: " + negocios[i].getNombreNegocio() + ")"
+                                    + "\n2. Tipo de negocio (Actual: " + negocios[i].getTipoNegocio() + ")"
+                                    + "\n3. Contacto (Actual: " + negocios[i].getContactoNegocio() + ")"
+                                    + "\n4. Teléfono (Actual: " + negocios[i].getTelefonoNegocio() + ")"
+                                    + "\n5. Correo (Actual: " + negocios[i].getCorreoNegocio() + ")"
+                                    + "\n6. Dirección (Actual: " + negocios[i].getDireccionNegocio() + ")"
+                                    + "\n7. Límite de crédito ($) (Actual: " + negocios[i].getLimiteCreditoNegocio() + ")"
+                                    + "\n8. Regresar"
+                            ));
+
+                    }
+                } while (opt != 8);
             }
         }
+
+    }
+
+    public void eliminarNegocio(Negocio[] negocios) {
+
     }
 }
